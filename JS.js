0b3d5f1f86d647da05eb688013ddaecb2d96ff20
GrProgrammer <joepetrosyan@mail.ru>
2017-05-08 01:26:27 +0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,11 +1,11 @@
 var mBlog = document.getElementById("mainBlog");
 var aPage = document.getElementById("abtPage");
 aPage.click = function myFunction(){
-  if(mBlog.className == "mainDiv"){
-    mBlog.className = "mainDivBlock";
-    aPage.className = "aboutPageShadow";
-  } else{
-    mBlog.className == "mainDiv";
+  if(mBlog.className == "mainDivBlock"){
+    mBlog.className = "";
     aPage.className = "aboutPage";
+  } else{
+    mBlog.className == "mainDivBlock";
+    aPage.className = "aboutPageShadow";
   }
 };
