e8cc404f367ffc7c02d67d58f36344eaea21fd39
GrProgrammer <joepetrosyan@mail.ru>
2017-05-08 11:56:48 +0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,6 +1,6 @@
 var mBlog = document.getElementById("mainBlog");
 var aPage = document.getElementById("abtPage");
 aPage.click = function myFunction(){
-  mBlog.setAttribute("style";"display: none";);
+  mBlog.setAttribute("style", "display: none";);
 };
 myFunction();
